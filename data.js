24e294b0ec21febf5c766b42bf186ab3d7c6939e
David Ortiz <david.ortiz.p@gmail.com>
2023-09-17 16:11:58 -0500

    data changed to transactions and methods where updated, except update transaction

--- data.js
+++ data.js
@@ -1,24 +1,60 @@
+// module.exports = {
+// users : {
+//     1: {
+//       id: '1',
+//       username: 'Robin Wieruch',
+//     },
+//     2: {
+//       id: '2',
+//       username: 'Dave Davids',
+//     },
+//   },
+//   messages : {
+//     1: {
+//       id: '1',
+//       text: 'Hello World',
+//       userId: '1',
+//     },
+//     2: {
+//       id: '2',
+//       text: 'By World',
+//       userId: '2',
+//     },
+//   }
+// }
+
 module.exports = {
-users : {
-    1: {
-      id: '1',
-      username: 'Robin Wieruch',
-    },
-    2: {
-      id: '2',
-      username: 'Dave Davids',
-    },
+transactions : [
+  {
+      id : 1,
+      type : "expense",
+      category : "food",
+      amount : 50,
   },
-  messages : {
-    1: {
-      id: '1',
-      text: 'Hello World',
-      userId: '1',
-    },
-    2: {
-      id: '2',
-      text: 'By World',
-      userId: '2',
-    },
-  }
-}
+  {
+      id : 2,
+      type : "expense",
+      category : "food",
+      amount : 150,
+  },
+ {
+      id : 3,
+      type : "expense",
+      category : "doctor",
+      amount : 280,
+  },
+ {
+      id : 4,
+      type : "income",
+      category : "salary",
+      amount : 1500,
+  },
+ {
+      id : 5,
+      type : "expense",
+      category : "taxi",
+      amount : 25,
+  },
+]
+} 
+
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const {users, messages} = require("./data.js")
+// const {users, messages} = require("./data.js
+let {transactions} = require("./data.js")
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 9000;
+
+app.use(express.json())
  
   // route: root
 app.get('/', (req, res) => {
@@ -21,34 +24,45 @@ app.delete('/', (req, res) => {
     return res.send('Received a DELETE HTTP method')
 });
 
-// route: users
-app.get('/users', (req,res) => {
-    return res.send(Object.values(users))
+// route: transactions
+app.get('/transactions', (req,res) => {
+    return res.json(Object.values(transactions))
 })
 
-app.get('/users/:userId', (req,res) => {
-    return res.send(users[req.params.userId])
+app.get('/transaction/:transactionId', (req,res) => {
+    return res.json(transactions.filter(transaction => transaction.id === parseInt(req.params.transactionId)))
 })
 
-app.post('/users', (req,res) => {
-    return res.send('POST HTTP method on user resource') })
+app.post('/transactions', (req,res) => {
+    const newId = transactions.reduce((acc,item,id)=> (item.id > acc)? item.id: acc ,0)
+    const {type, category, amount} = req.body
+    const newTransaction = {id:newId + 1, type, category, amount}
+    transactions.push(newTransaction)
+    console.log(newTransaction)
+
+    // console.log(req.body)
+    return res.send('created')
+})
 
 app.put('/users/:userId', (req,res) => {
     return res.send(`PUT HTTP method on user /${req.params.userId} resource`)
 })
 
-app.delete('/users/:userId', (req,res) => {
-    return res.send(`DELETE HTTP method on user /${req.params.userId} resource`)
+app.delete('/transaction/:transactionId', (req,res) => {
+    transactions = 
+        transactions.filter(transaction => transaction.id !== parseInt(req.params.transactionId))
+
+    return res.send(`DELETED transaction with Id : ${req.params.transactionId}`)
 })
 
-// route : messages
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages))
-})
+// // route : messages
+// app.get('/messages', (req, res) => {
+//     return res.send(Object.values(messages))
+// })
 
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId])
-})
+// app.get('/messages/:messageId', (req, res) => {
+//     return res.send(messages[req.params.messageId])
+// })
 
 const handleHello = (req,res) => {
   res.send('hello world')
