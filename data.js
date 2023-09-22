1aa2d5af681bda9a212b7ce7d4f91c1cf523d8fe
David Ortiz <david.ortiz.p@gmail.com>
2023-09-21 19:35:09 -0500

    fixed missing id in data

--- data.js
+++ data.js
@@ -23,38 +23,341 @@
 //   }
 // }
 
+// module.exports = {
+// transactions : [
+//   {
+// //       id : 1,
+//       type : "expense",
+//       category : "food",
+//       amount : 50,
+//   },
+//   {
+//       id : 2,
+//       type : "expense",
+//       category : "food",
+//       amount : 150,
+//   },
+//  {
+//       id : 3,
+//       type : "expense",
+//       category : "doctor",
+//       amount : 280,
+//   },
+//  {
+//       id : 4,
+//       type : "income",
+//       category : "salary",
+//       amount : 1500,
+//   },
+//  {
+//       id : 5,
+//       type : "expense",
+//       category : "taxi",
+//       amount : 25,
+//   },
+// ]
+// } 
 module.exports = {
-transactions : [
-  {
-      id : 1,
-      type : "expense",
-      category : "food",
-      amount : 50,
-  },
-  {
-      id : 2,
-      type : "expense",
-      category : "food",
-      amount : 150,
-  },
- {
-      id : 3,
-      type : "expense",
-      category : "doctor",
-      amount : 280,
-  },
- {
-      id : 4,
-      type : "income",
-      category : "salary",
-      amount : 1500,
-  },
- {
-      id : 5,
-      type : "expense",
-      category : "taxi",
-      amount : 25,
-  },
-]
-} 
-
+  transactions: [
+    {
+      id: 1,
+      type: "expense",
+      category: "food",
+      amount: 50,
+    },
+    {
+      id: 2,
+      type: "expense",
+      category: "food",
+      amount: 150,
+    },
+    {
+      id: 3,
+      type: "expense",
+      category: "doctor",
+      amount: 280,
+    },
+    {
+      id: 4,
+      type: "income",
+      category: "salary",
+      amount: 1500,
+    },
+    {
+      id: 5,
+      type: "expense",
+      category: "taxi",
+      amount: 25,
+    },
+    {
+      id: 6,
+      type: "income",
+      category: "freelance",
+      amount: 800,
+    },
+    {
+      id: 7,
+      type: "expense",
+      category: "entertainment",
+      amount: 75,
+    },
+    {
+      id: 8,
+      type: "expense",
+      category: "food",
+      amount: 60,
+    },
+    {
+      id: 9,
+      type: "expense",
+      category: "doctor",
+      amount: 200,
+    },
+    {
+      id: 10,
+      type: "income",
+      category: "salary",
+      amount: 1600,
+    },
+    {
+      id: 11,
+      type: "expense",
+      category: "taxi",
+      amount: 30,
+    },
+    {
+      id: 12,
+      type: "income",
+      category: "freelance",
+      amount: 850,
+    },
+    {
+      id: 13,
+      type: "expense",
+      category: "entertainment",
+      amount: 70,
+    },
+    {
+      id: 14,
+      type: "expense",
+      category: "food",
+      amount: 55,
+    },
+    {
+      id: 15,
+      type: "expense",
+      category: "doctor",
+      amount: 290,
+    },
+    {
+      id: 16,
+      type: "income",
+      category: "salary",
+      amount: 1550,
+    },
+    {
+      id: 17,
+      type: "expense",
+      category: "taxi",
+      amount: 28,
+    },
+    {
+      id: 18,
+      type: "income",
+      category: "freelance",
+      amount: 820,
+    },
+    {
+      id: 19,
+      type: "expense",
+      category: "entertainment",
+      amount: 72,
+    },
+    {
+      id: 20,
+      type: "expense",
+      category: "food",
+      amount: 58,
+    },
+    {
+      id: 21,
+      type: "expense",
+      category: "doctor",
+      amount: 270,
+    },
+    {
+      id: 22,
+      type: "income",
+      category: "salary",
+      amount: 1480,
+    },
+    {
+      id: 23,
+      type: "expense",
+      category: "taxi",
+      amount: 23,
+    },
+    {
+      id: 24,
+      type: "income",
+      category: "freelance",
+      amount: 810,
+    },
+    {
+      id: 25,
+      type: "expense",
+      category: "entertainment",
+      amount: 73,
+    },
+    {
+      id: 26,
+      type: "expense",
+      category: "food",
+      amount: 57,
+    },
+    {
+      id: 27,
+      type: "expense",
+      category: "doctor",
+      amount: 285,
+    },
+    {
+      id: 28,
+      type: "income",
+      category: "salary",
+      amount: 1530,
+    },
+    {
+      id: 29,
+      type: "expense",
+      category: "taxi",
+      amount: 26,
+    },
+    {
+      id: 30,
+      type: "income",
+      category: "freelance",
+      amount: 830,
+    },
+    {
+      id: 31,
+      type: "expense",
+      category: "entertainment",
+      amount: 74,
+    },
+    {
+      id: 32,
+      type: "expense",
+      category: "food",
+      amount: 53,
+    },
+    {
+      id: 33,
+      type: "expense",
+      category: "doctor",
+      amount: 295,
+    },
+    {
+      id: 34,
+      type: "income",
+      category: "salary",
+      amount: 1580,
+    },
+    {
+      id: 35,
+      type: "expense",
+      category: "taxi",
+      amount: 29,
+    },
+    {
+      id: 36,
+      type: "income",
+      category: "freelance",
+      amount: 840,
+    },
+    {
+      id: 37,
+      type: "expense",
+      category: "entertainment",
+      amount: 71,
+    },
+    {
+      id: 38,
+      type: "expense",
+      category: "food",
+      amount: 52,
+    },
+    {
+      id: 39,
+      type: "expense",
+      category: "doctor",
+      amount: 270,
+    },
+    {
+      id: 40,
+      type: "income",
+      category: "salary",
+      amount: 1470,
+    },
+    {
+      id: 41,
+      type: "expense",
+      category: "taxi",
+      amount: 22,
+    },
+    {
+      id: 42,
+      type: "income",
+      category: "freelance",
+      amount: 800,
+    },
+    {
+      id: 43,
+      type: "expense",
+      category: "entertainment",
+      amount: 76,
+    },
+    {
+      id: 44,
+      type: "expense",
+      category: "food",
+      amount: 54,
+    },
+    {
+      id: 45,
+      type: "expense",
+      category: "doctor",
+      amount: 260,
+    },
+    {
+      id: 46,
+      type: "income",
+      category: "salary",
+      amount: 1450,
+    },
+    {
+      id: 47,
+      type: "expense",
+      category: "taxi",
+      amount: 24,
+    },
+    {
+      id: 48,
+      type: "income",
+      category: "freelance",
+      amount: 790,
+    },
+    {
+      id: 49,
+      type: "expense",
+      category: "entertainment",
+      amount: 78,
+    },
+    {
+      id: 50,
+      type: "expense",
+      category: "food",
+      amount: 59,
+    },
+  ],
+};
